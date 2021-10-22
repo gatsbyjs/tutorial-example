910faa5b286048592f10ed83a062a352d84adbfc
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 21:13:05 -0700

    feat(part-6): create blog post page template

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>My blog post contents will go here (eventually).</p>
+    </Layout>
+  )
+}
+
+export default BlogPost
