03e0b6b0e45d898d07d5032efc1ccbf3deb9ab82
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 21:19:50 -0700

    feat(part-6): render post contents in blog post page template

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,29 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
 
-const BlogPost = () => {
+const BlogPost = ({ data }) => {
   return (
-    <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
 
 export default BlogPost
