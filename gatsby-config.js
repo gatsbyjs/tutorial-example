f7dc93f5fd1fe2ed0165a0f25b29a91fbf4d60f7
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 21:00:55 -0700

    feat(part-4): configure gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
