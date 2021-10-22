aed7e2046ef6ce5ecbdf88555f57e68b7804a9ec
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 20:31:10 -0700

    feat(part-3): add image from filesystem instead of from external URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="../images/clifford.jpg"
       />
     </Layout>
   )
