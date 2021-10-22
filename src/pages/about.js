72e60277e74235be1ade8c85c374e6295411562c
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 20:16:43 -0700

    feat(part-2): create a new about page component

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
