6c209705a22620356530b7804b55807283024e96
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 21:04:27 -0700

    feat(part-4): add list of post filenames to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,31 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage
