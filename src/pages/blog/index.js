56b281c56ceadfa30535802038126e1eb8d4ab9a
Megan Sullivan <megan@gatsbyjs.com>
2021-10-21 21:22:24 -0700

    feat(part-6): update blog page to link to each post

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,19 +1,19 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
@@ -30,7 +30,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
